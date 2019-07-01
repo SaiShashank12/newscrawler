825688218b47363f66d68989f7faca2b3d078da4
Sai Shashank Mudliar <34842779+The-crazy-one@users.noreply.github.com>
2019-07-01 15:27:47 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,22 +4,6 @@ from downtoearth import run as de
 import threading as th
 
 
-t1 = th.Thread(target=sm)
-t2 = th.Thread(target=al)
-t3 = th.Thread(target=de)
-
-try:
-    t1.start()
-except:
-    print('t1')    
-try:
-    t2.start()
-except:
-    print('t2')
-try:
-    t3.start()
-except:
-    print('t3')   
-t1.join()
-t2.join()
-t3.join()
+sm()
+al()
+de()
